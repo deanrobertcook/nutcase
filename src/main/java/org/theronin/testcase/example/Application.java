c7fb34d732881dddd861aafc10cb949a739fcf94
deanrobertcook <deanrobertcook@gmail.com>
2015-11-14 21:29:29 +0100

    Improve the JDBC example by following the JPA example

--- src/main/java/org/theronin/testcase/example/Application.java
+++ src/main/java/org/theronin/testcase/example/Application.java
@@ -2,21 +2,16 @@ package org.theronin.testcase.example;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.jdbc.core.JdbcTemplate;
-
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
+import org.springframework.context.annotation.Bean;
 
 /**
  *
  */
 @SpringBootApplication
-public class Application implements CommandLineRunner {
+public class Application {
 
     private static final Logger log = LoggerFactory.getLogger(Application.class);
 
@@ -24,28 +19,36 @@ public class Application implements CommandLineRunner {
         SpringApplication.run(Application.class, args);
     }
 
-    @Autowired
-    JdbcTemplate jdbcTemplate;
+    @Bean
+    public CommandLineRunner demo(CustomerRepository repository) {
+        return (args -> {
+            repository.save(new Customer("Jack", "Bauer"));
+            repository.save(new Customer("Chloe", "O'brian"));
+            repository.save(new Customer("Kim", "Bauer"));
+            repository.save(new Customer("David", "Palmer"));
+            repository.save(new Customer("Michelle", "Dessler"));
 
-    @Override
-    public void run(String... args) throws Exception {
-        log.info("Creating tables");
+            log.info("Customers found with findAll()");
+            log.info("------------------------------");
+            for (Customer customer: repository.findAll()) {
+                log.info(customer.toString());
+            }
 
-        jdbcTemplate.execute("DROP TABLE customers IF EXISTS");
-        jdbcTemplate.execute("CREATE TABLE customers(id SERIAL, first_name VARCHAR(255), last_name VARCHAR(255))");
+            log.info("");
 
-        List<Object[]> splitUpNames = Arrays.asList("John Woo", "Jeff Dean", "Josh Bloch", "Josh Long").stream()
-                .map(name -> name.split(" "))
-                .collect(Collectors.toList());
+            Customer customer = repository.findOne(1L);
+            log.info("Customer found with findOne(1L)");
+            log.info("------------------------------");
+            log.info(customer.toString());
 
-        splitUpNames.forEach(name -> log.info(String.format("Inserting customer record for %s %s", name[0], name[1])));
+            log.info("");
 
-        jdbcTemplate.batchUpdate("INSERT INTO customers(first_name, last_name) VALUES (?, ?)", splitUpNames);
-
-        log.info("Querying for customer records where first_name = 'Josh':");
-        jdbcTemplate.query("SELECT id, first_name, last_name FROM customers WHERE first_name = ?",
-                new Object[] {"Josh"},
-                (rs, rowNum) -> new Customer(rs.getLong("id"), rs.getString("first_name"), rs.getString("last_name")))
-        .forEach(customer -> log.info(customer.toString()));
+            log.info("Customers found with findByLastName('Bauer')");
+            log.info("------------------------------");
+            for (Customer bauer: repository.findByLastName("Bauer")) {
+                log.info(bauer.toString());
+            }
+            log.info("");
+        });
     }
 }
--- src/main/java/org/theronin/testcase/example/Customer.java
+++ src/main/java/org/theronin/testcase/example/Customer.java
@@ -1,14 +1,25 @@
 package org.theronin.testcase.example;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 /**
  *
  */
+@Entity
 public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
     private String firstName;
     private String lastName;
 
-    public Customer(long id, String firstName, String lastName) {
+    protected Customer() {}
+
+    public Customer(String firstName, String lastName) {
         this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
