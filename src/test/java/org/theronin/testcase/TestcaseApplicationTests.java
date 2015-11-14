5fd3b1708836949fc12adddbde977cc8ba49cd45
deanrobertcook <deanrobertcook@gmail.com>
2015-11-14 19:28:37 +0100

    Initial commit with .gitignore

---
+++ src/test/java/org/theronin/testcase/TestcaseApplicationTests.java
@@ -0,0 +1,16 @@
+package org.theronin.testcase;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = TestcaseApplication.class)
+public class TestcaseApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
