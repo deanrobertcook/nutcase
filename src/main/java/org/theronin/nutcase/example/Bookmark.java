fe4913c921d30539efafc1fadde9462fbeda7e55
deanrobertcook <deanrobertcook@gmail.com>
2015-11-15 12:06:08 +0100

    Add tests for the full REST example

--- src/main/java/org/theronin/nutcase/example/Bookmark.java
+++ src/main/java/org/theronin/nutcase/example/Bookmark.java
@@ -19,7 +19,7 @@ public class Bookmark {
 
     @Id
     @GeneratedValue
-    private long id;
+    private Long id;
 
     private String uri;
     private String description;
@@ -40,11 +40,11 @@ public class Bookmark {
         this.account = account;
     }
 
-    public long getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(long id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
--- src/main/java/org/theronin/nutcase/example/BookmarkRestController.java
+++ src/main/java/org/theronin/nutcase/example/BookmarkRestController.java
@@ -54,6 +54,7 @@ public class BookmarkRestController {
     }
 }
 
+@ResponseStatus(HttpStatus.NOT_FOUND)
 class UserNotFoundException extends RuntimeException {
     public UserNotFoundException(String userId) {
         super("could not find user '" + userId + "'.");
