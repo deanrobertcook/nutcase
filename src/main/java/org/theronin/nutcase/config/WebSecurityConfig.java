e51278a4fa69d55e3aa8286ab6024c0995554c74
deanrobertcook <deanrobertcook@gmail.com>
2015-11-15 20:27:03 +0100

    Add WebSecurityConfig to project

---
+++ src/main/java/org/theronin/nutcase/config/WebSecurityConfig.java
@@ -0,0 +1,22 @@
+package org.theronin.nutcase.config;
+
+/**
+ *
+ */
+//@Configuration
+//@EnableWebMvcSecurity
+//public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.authorizeRequests()
+//                .antMatchers("/", "users").permitAll().anyRequest().authenticated().and()
+//                .formLogin().loginPage("/#!").permitAll().and()
+//                .logout().permitAll();
+//
+//    }
+//
+//    @Autowired
+//    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.inMemoryAuthentication().withUser("user").password("password").roles("USER");
+//    }
+//}
