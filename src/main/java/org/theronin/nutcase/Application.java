4b679c38c75f8f7316339eb86b7f948bbcb417e6
Michael Koppen <michael.koppen@googlemail.com>
2015-11-19 22:31:35 +0100

    Cleanup & preperation for caching and locales

--- src/main/java/org/theronin/nutcase/Application.java
+++ src/main/java/org/theronin/nutcase/Application.java
@@ -1,10 +1,19 @@
 package org.theronin.nutcase;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Arrays;
+import java.util.Collection;
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.core.env.Environment;
+import org.springframework.core.env.SimpleCommandLinePropertySource;
+import org.theronin.nutcase.config.Constants;
 import org.theronin.nutcase.config.NCProperties;
 
 @SpringBootApplication
@@ -13,8 +22,49 @@ public class Application {
 
 		private static final Logger log = LoggerFactory.getLogger(Application.class);
 
-		public static void main(String[] args) {
-				SpringApplication.run(Application.class, args);
+		@Inject
+		private Environment env;
+
+		/**
+		 * <p/>
+		 * Spring profiles can be configured with arguments
+		 * --spring.profiles.active=your-active-profile
+		 * <p/>
+		 */
+		@PostConstruct
+		public void initApplication() {
+				if (env.getActiveProfiles().length == 0) {
+						log.warn("No Spring profile configured, running with default configuration");
+				} else {
+						log.info("Running with Spring profile(s) : {}", Arrays.toString(env.getActiveProfiles()));
+						Collection<String> activeProfiles = Arrays.asList(env.getActiveProfiles());
+						if (activeProfiles.contains(Constants.SPRING_PROFILE_DEVELOPMENT) && activeProfiles.contains(Constants.SPRING_PROFILE_PRODUCTION)) {
+								log.error("You have misconfigured your application! "
+												+ "It should not run with both the 'dev' and 'prod' profiles at the same time.");
+						}
+				}
+		}
+
+		public static void main(String[] args) throws UnknownHostException {
+				SpringApplication app = new SpringApplication(Application.class);
+				SimpleCommandLinePropertySource source = new SimpleCommandLinePropertySource(args);
+				addDefaultProfile(app, source);
+				Environment env = app.run(args).getEnvironment();
+				log.info("Access URLs:\n----------------------------------------------------------\n\t"
+								+ "Local: \t\thttp://127.0.0.1:{}\n\t"
+								+ "External: \thttp://{}:{}\n----------------------------------------------------------",
+								env.getProperty("server.port"),
+								InetAddress.getLocalHost().getHostAddress(),
+								env.getProperty("server.port"));
+
+		}
+
+		private static void addDefaultProfile(SpringApplication app, SimpleCommandLinePropertySource source) {
+				if (!source.containsProperty("spring.profiles.active")
+								&& !System.getenv().containsKey("SPRING_PROFILES_ACTIVE")) {
+
+						app.setAdditionalProfiles(Constants.SPRING_PROFILE_DEVELOPMENT);
+				}
 		}
 
 }
---
+++ src/main/java/org/theronin/nutcase/config/web/WebConfigurer.java
@@ -0,0 +1,56 @@
+package org.theronin.nutcase.config.web;
+
+import java.util.Arrays;
+import java.util.EnumSet;
+import javax.inject.Inject;
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.context.embedded.ServletContextInitializer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.theronin.nutcase.config.Constants;
+import org.theronin.nutcase.config.cache.CacheConfiguration;
+import org.theronin.nutcase.config.web.filter.CachingHttpHeadersFilter;
+
+@Configuration
+@AutoConfigureAfter(CacheConfiguration.class)
+public class WebConfigurer implements ServletContextInitializer {
+
+		private final Logger log = LoggerFactory.getLogger(WebConfigurer.class);
+
+		@Inject
+		private Environment env;
+
+		@Override
+		public void onStartup(ServletContext servletContext) throws ServletException {
+				log.info("Web application configuration, using profiles: {}", Arrays.toString(env.getActiveProfiles()));
+				EnumSet<DispatcherType> disps = EnumSet.of(DispatcherType.REQUEST, DispatcherType.FORWARD, DispatcherType.ASYNC);
+				if (env.acceptsProfiles(Constants.SPRING_PROFILE_PRODUCTION)) {
+						initCachingHttpHeadersFilter(servletContext, disps);
+				}
+				if (env.acceptsProfiles(Constants.SPRING_PROFILE_DEVELOPMENT)) {
+						// add dev config here
+				}
+				log.info("Web application fully configured");
+		}
+
+		/**
+		 * Initializes the caching HTTP Headers Filter.
+		 */
+		private void initCachingHttpHeadersFilter(ServletContext servletContext,
+						EnumSet<DispatcherType> disps) {
+				log.debug("Registering Caching HTTP Headers Filter");
+				FilterRegistration.Dynamic cachingHttpHeadersFilter
+								= servletContext.addFilter("cachingHttpHeadersFilter",
+												new CachingHttpHeadersFilter(env));
+
+				cachingHttpHeadersFilter.addMappingForUrlPatterns(disps, true, "/assets/*");
+				cachingHttpHeadersFilter.addMappingForUrlPatterns(disps, true, "/scripts/*");
+				cachingHttpHeadersFilter.setAsyncSupported(true);
+		}
+}
